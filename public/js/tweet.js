a2dc062c677f01c13a73df089bbf96376e20b257
omarsagoo <om.sagoo@gmail.com>
2020-12-11 19:20:38 -0800

    [add] latest search options, add slider with custom CSS, add JS for latest search

--- public/js/tweet.js
+++ public/js/tweet.js
@@ -1,4 +1,18 @@
 $(document).ready(() => {
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    output.innerHTML = "Last " + slider.value + " hours"; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() {
+        output.innerHTML = "Last " + this.value + " hours";
+    }
+
+    $("#latestButton").on("click", event => {
+        event.preventDefault();
+        window.location.replace("/latest/"+slider.value)
+    })
+
     $("#tweetModal").on("show.bs.modal", event => {
         document.getElementById("container").innerHTML = `<div class="text-center">
         <div class="spinner-border" role="status">
